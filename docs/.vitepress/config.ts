464e22c40d9d39e63367ab9a4cee74d240ac272b
aristidelegba <aristidelegba67@gmail.com>
2023-12-25 14:22:56 +0100

    wip

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,7 +2,7 @@ import { createRequire } from 'module'
 import { defineConfig, type DefaultTheme } from 'vitepress'
 
 const require = createRequire(import.meta.url)
-const pkg = require('../../package.json')
+import pkg from '../../package.json'
 
 export default defineConfig({
   lang: 'en-US',
